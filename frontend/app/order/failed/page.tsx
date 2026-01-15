5f51e48033f1eb4020be8b8eb0670ce5141ca7ce
abhinav0014 <agyawali78@gmail.com>
2026-01-15 11:44:10 +0545

    made order fsil and success just to run

---
+++ frontend/app/order/failed/page.tsx
@@ -0,0 +1,10 @@
+// app/account/page.tsx
+import React from "react";
+
+export default function OrderFailed() {
+  return (
+    <div>
+      <h1>order failed Page</h1>
+    </div>
+  );
+}
---
+++ frontend/app/order/success/page.tsx
@@ -0,0 +1,10 @@
+// app/account/page.tsx
+import React from "react";
+
+export default function OrderSuccess() {
+  return (
+    <div>
+      <h1>order Successful Page</h1>
+    </div>
+  );
+}
